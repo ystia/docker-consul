3f90fd0a62dff2640de9a082f5cfea189c383a0f
Albertin Loic <loic.albertin@atos.net>
2021-01-06 17:31:10 +0100

    Upgrade alpine version from 3.6 to 3.12

--- alpine-consul/build.sh
+++ alpine-consul/build.sh
@@ -5,4 +5,4 @@ cd ${script_dir}
 docker build $@ \
     --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
     --build-arg VCS_REF=`git rev-parse --short HEAD` \
-    -t loicalbertin/alpine-consul .
+    -t ystia/alpine-consul .
